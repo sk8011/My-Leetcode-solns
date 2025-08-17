0171cd2f02e8912626f285744e5dea019ee1407d
Saurav007 <142532932+sk8011@users.noreply.github.com>
2025-08-17 23:33:00 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,50 @@
+// Last updated: 17/8/2025, 11:33:02 pm
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> ans;
+    queue<TreeNode*> qq;
+    void bfs(TreeNode* root){
+        if(root!=NULL){
+            qq.push(root);
+        }
+        vector<int> lvl;
+        int temp=1;
+        int i=0;
+        int c=0;
+        while(!qq.empty()){
+            while(i<temp){
+                if(qq.front()->left!=NULL){
+                    qq.push(qq.front()->left);
+                    c++;
+                }
+                if(qq.front()->right!=NULL){
+                    qq.push(qq.front()->right);
+                    c++;
+                }
+                lvl.push_back(qq.front()->val);
+                qq.pop();
+                i++;
+            }
+            temp=c;
+            i=0;
+            c=0;
+            ans.push_back(lvl);
+            lvl.clear();
+        }
+    }
+    int maxDepth(TreeNode* root) {
+        bfs(root);
+        return ans.size();
+    }
+};
