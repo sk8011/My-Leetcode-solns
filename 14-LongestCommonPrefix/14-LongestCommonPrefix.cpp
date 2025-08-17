7396c2dc17099d270c790feffc23bf3c3b5175b7
Saurav007 <142532932+sk8011@users.noreply.github.com>
2025-08-17 23:33:57 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,22 @@
+// Last updated: 17/8/2025, 11:33:58 pm
+#include <vector>
+#include <string>
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = ""; int i = 0;
+
+        while (i < strs[0].size()) {
+            for (int j = 0; j < strs.size(); j++) {
+                if (strs[j][i] != strs[0][i]) {
+                    return prefix;
+                }
+            }
+            prefix += strs[0][i];  
+            i++;
+        }
+
+        return prefix;
+    }
+};
