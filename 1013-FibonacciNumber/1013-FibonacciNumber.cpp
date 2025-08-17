44c598f1d7de6513495d82cac05ca65e0d6f8d1f
Saurav007 <142532932+sk8011@users.noreply.github.com>
2025-08-17 23:31:30 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 17/8/2025, 11:31:32 pm
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+};
