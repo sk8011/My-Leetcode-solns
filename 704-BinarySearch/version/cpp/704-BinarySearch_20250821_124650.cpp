afbf5304b9ce3199c24b80602e578f7113fbae8d
Saurav007 <142532932+sk8011@users.noreply.github.com>
2025-08-21 12:46:50 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/cpp/704-BinarySearch_20250821_124650.cpp
@@ -0,0 +1,31 @@
+// Last updated: 21/8/2025, 12:46:50 pm
+class Solution {
+public:
+    // int binSearch(vector<int> arr,int& left,int& mid,int& right,int target){
+    //     if(left>right) return -1;
+    //     if(target==arr[mid]) return mid;
+    //     else if(target>arr[mid]){
+    //         left=mid+1;
+    //         mid=(left+right)/2;
+    //     }else{
+    //         right=mid-1;
+    //         mid=(left+right)/2;
+    //     }
+    //     return binSearch(arr,left,mid,right,target);
+    // }
+    int search(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int mid=(left+right)/2;
+        while(right>=left){
+            if(target==nums[mid]) return mid;
+            else if(target>nums[mid]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            mid=(left+right)/2;
+        }
+        return -1;
+    }
+};
