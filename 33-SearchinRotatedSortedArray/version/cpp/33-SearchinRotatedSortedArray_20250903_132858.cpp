deb05ef791177c169a1ae2d6a963ed3d5f2ff122
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-03 13:28:58 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/version/cpp/33-SearchinRotatedSortedArray_20250903_132858.cpp
@@ -0,0 +1,35 @@
+// Last updated: 3/9/2025, 1:28:58 pm
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int high=n-1;
+        int low=0;
+        while(high>=low){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target){
+                if(nums[low]>nums[mid]){
+                    high=mid-1;
+                }else{
+                    if(nums[low]>target){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                    }
+                }
+            }else{
+                if(nums[mid]>nums[high]){
+                    low=mid+1;
+                }else{
+                    if(target>nums[high]){
+                        high=mid-1;
+                    }else{
+                        low=mid+1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
