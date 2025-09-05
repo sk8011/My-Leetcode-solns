f2763352665eed637aab51d3fd95bb9241f367b5
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-06 00:40:34 +0530

    Create 1781-SumofBeautyofAllSubstrings

---
+++ 1781-SumofBeautyofAllSubstrings/version/cpp/1781-SumofBeautyofAllSubstrings_20250906_004033.cpp
@@ -0,0 +1,40 @@
+// Last updated: 6/9/2025, 12:40:33 am
+class Solution {
+public:
+    int beautySum(string s) {
+        // for(char c:s){
+        //     freq[c]++;
+        // }
+        int sum=0;
+        int wSize=3;
+        int n=s.size();
+        if(n<=1) return 0;
+        vector<int> freq(26,0);
+        for(int i=0;i<wSize-1;i++){
+            freq[s[i]-'a']++;
+        }
+        vector<int> temp;
+        while(wSize<n+1){
+            int start=0;
+            int end=start+wSize-1;
+            freq[s[end]-'a']++;
+            temp=freq;
+            while(end<n){
+                int maxf=*max_element(freq.begin(),freq.end());
+                int minf=maxf;
+                for(int e:freq){
+                    if(e!=0) minf=min(minf,e);
+                }
+                sum+=maxf-minf;
+                freq[s[start]-'a']--;
+                start++;
+                end++;
+                if(end!=n)freq[s[end]-'a']++;
+            }
+            // freq.clear();
+            freq=temp;
+            wSize++;
+        }
+        return sum;
+    }
+};
