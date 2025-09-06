0936a9ce78a23f04705f3503fbe52dd078f6c940
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-06 17:01:42 +0530

    Create 1283-FindtheSmallestDivisorGivenaThreshold

---
+++ 1283-FindtheSmallestDivisorGivenaThreshold/version/cpp/1283-FindtheSmallestDivisorGivenaThreshold_20250906_170142.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/9/2025, 5:01:42 pm
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1;
+        int high=1000000;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            int sum=0;
+            for(int e:nums){
+                sum+=(e+mid-1)/mid;
+            }
+            if(sum<=threshold){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return high;
+    }
+};
