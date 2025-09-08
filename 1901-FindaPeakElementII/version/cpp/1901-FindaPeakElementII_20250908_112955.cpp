0de94d9768773808a08442d01c03ead1984169b5
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-08 11:29:55 +0530

    Create 1901-FindaPeakElementII

---
+++ 1901-FindaPeakElementII/version/cpp/1901-FindaPeakElementII_20250908_112955.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/9/2025, 11:29:55 am
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                bool check=true;
+                if(i+1<m){
+                    if(mat[i][j]<=mat[i+1][j]) check=false;
+                }
+                if(i-1>=0){
+                    if(mat[i][j]<=mat[i-1][j]) check=false;
+                }
+                if(j+1<n){
+                    if(mat[i][j]<=mat[i][j+1]) check=false;
+                }
+                if(j-1>=0){
+                    if(mat[i][j]<=mat[i][j-1]) check=false;
+                }
+                if(check) return {i,j};
+            }
+        }
+        return {-1,-1};
+    }
+};
