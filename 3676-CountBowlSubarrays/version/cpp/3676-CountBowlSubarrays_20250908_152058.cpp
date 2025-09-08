644d055dbe56940743b08c8869e3febdc59a6142
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-08 15:20:58 +0530

    Create 3676-CountBowlSubarrays

---
+++ 3676-CountBowlSubarrays/version/cpp/3676-CountBowlSubarrays_20250908_152058.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/9/2025, 3:20:58 pm
+class Solution {
+public:
+    long long bowlSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        stack<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!s.empty() && nums[s.top()] < nums[i]) {
+                s.pop();
+                if (!s.empty()) {
+                    ans++;
+                }
+            }
+            s.push(i);
+        }
+
+        return ans;
+    }
+};
