bfc1d2ce71e21c44a92b744a1d4ac51efb03538b
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-09 10:06:13 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/version/cpp/198-HouseRobber_20250909_100612.cpp
@@ -0,0 +1,21 @@
+// Last updated: 9/9/2025, 10:06:12 am
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        int prev1=nums[0];
+        int prev2,curr;
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1){
+                pick+=prev2;
+            }
+            int nonPick=prev1;
+            curr=max(nonPick,pick);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
