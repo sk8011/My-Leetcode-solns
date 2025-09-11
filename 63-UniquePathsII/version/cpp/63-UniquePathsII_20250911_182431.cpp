70a998143923d356ccdb0443c5b6f4fc2734aa60
Saurav Kumar <142532932+sk8011@users.noreply.github.com>
2025-09-11 18:24:31 +0530

    Create 63-UniquePathsII

---
+++ 63-UniquePathsII/version/cpp/63-UniquePathsII_20250911_182431.cpp
@@ -0,0 +1,25 @@
+// Last updated: 11/9/2025, 6:24:31 pm
+class Solution {
+public:
+    int countWays(int i, int j, vector<vector<int>> &dp, vector<vector<int>> &obs){
+        if((i>=0 && j>=0) && obs[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int up=countWays(i-1,j,dp,obs);
+        int left=countWays(i,j-1,dp,obs);
+
+        return dp[i][j]=up+left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return countWays(m-1,n-1,dp,obstacleGrid);
+    }
+};
