484a0ec1093e0a8eed4bd73567c92244ec92b140
Saurav Kumar <sauravkvrdsoviiib2512@gmail.com>
2025-11-04 10:29:51 +0530

    Create 1092-ShortestCommonSupersequence

---
+++ 1092-ShortestCommonSupersequence/version/cpp/1092-ShortestCommonSupersequence_20251104_102952.cpp
@@ -0,0 +1,43 @@
+// Last updated: 4/11/2025, 10:29:52 am
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n1 = str1.size(), n2 = str2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i=n1,j=n2;
+        string ans;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
+                i--; j--;
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1[i-1];
+                i--;
+            }else{
+                ans+=str2[j-1];
+                j--;
+            }
+        }
+        while(i>0){
+            ans+=str1[i-1];
+            i--;
+        }
+        while(j>0){
+            ans+=str2[j-1];
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
