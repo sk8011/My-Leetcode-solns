0b0d3476246ff5a4a53fba420d71e69c7489239e
Saurav Kumar <sauravkvrdsoviiib2512@gmail.com>
2025-11-05 17:42:02 +0530

    Create 90-SubsetsII

---
+++ 90-SubsetsII/version/cpp/90-SubsetsII_20251105_174202.cpp
@@ -0,0 +1,33 @@
+// Last updated: 5/11/2025, 5:42:02 pm
+// class Solution {
+// public:
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+//     }
+// };
+
+class Solution {
+public:
+    void recur(int ind, vector<int> nums, vector<int> temp, set<vector<int>>& ans){
+        if(ind==-1){
+            ans.insert(temp);
+            return;
+        }
+
+        temp.push_back(nums[ind]);
+        recur(ind-1,nums,temp,ans);
+        temp.pop_back();
+        
+        recur(ind-1,nums,temp,ans);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> ans;
+        vector<int> temp;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        recur(n-1,nums,temp,ans);
+        vector<vector<int>> ansR(ans.begin(),ans.end());
+        return ansR;
+    }
+};
