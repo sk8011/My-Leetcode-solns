15d22993e4451721665af24ed8746cdc85e0ea3a
Saurav Kumar <sauravkvrdsoviiib2512@gmail.com>
2025-11-05 12:36:12 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/cpp/20-ValidParentheses_20251105_123613.cpp
@@ -0,0 +1,23 @@
+// Last updated: 5/11/2025, 12:36:13 pm
+class Solution {
+public:
+
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+                st.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
